e54a0a02cd76d7ae88df52d6611672218b9df174
sader <sader@jrs>
2025-12-06 14:39:24 +0100

    boh

--- test2.py
+++ test2.py
@@ -65,8 +65,11 @@ queries_names = [q[0].replace("MESH:", "") for q in train_queries]
 queries_names[:10]
 
 dictionary_names_normal, dictionary_cuis, dictionary_names_annotated = load_dictionary(cfg.paths.dictionary_raw_path, 
-                                special_token_start=mention_start_special_token, 
-                                special_token_end=mention_end_special_token,
-                            dictionary_max_chars_length=dictionary_max_chars_length,
-                            add_synonyms=bool(cfg.tokenize.dictionaries_annotate and cfg.tokenize.dictionary_annotation_add_synonyms)
-                                )
+        special_token_start=mention_start_special_token, 
+        special_token_end=mention_end_special_token,
+        dictionary_max_chars_length=dictionary_max_chars_length,
+        add_synonyms=bool(cfg.tokenize.dictionaries_annotate and cfg.tokenize.dictionary_annotation_add_synonyms)
+)
+
+dictionary_names_normal = [d.replace("MESH:", "") for d in dictionary_names_normal]
+dictionary_names_normal[:10]
